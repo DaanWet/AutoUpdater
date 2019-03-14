11299e31bd21cd76eeefebf65e76f291a1f14940
dwettinc <daan.wettinck@ugent.be>
2019-03-14 16:33:14 +0100

    Finished update (currently ) to test folder
    Not including hashes, just downloading all plugins except for spigot build

--- src/main/java/me/damascus2000/autoupdater/Updater.java
+++ src/main/java/me/damascus2000/autoupdater/Updater.java
@@ -7,23 +7,27 @@ import org.json.simple.parser.JSONParser;
 
 import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Updater {
 
 
     public static void update() throws MalformedURLException {
-        URL lijst = new URL("https://users.ugent.be/~fldbossc/");
+        URL lijst = new URL("https://users.ugent.be/~fldbossc/cgi-bin/hash.py");
         try{
             String jsonString = new Scanner(lijst.openStream(), "UTF-8").useDelimiter("\\A").next();
+            Bukkit.getLogger().info(jsonString);
             JSONParser parser = new JSONParser();
             JSONObject jsonObject = (JSONObject) parser.parse(jsonString);
-            for (Object object : jsonObject.values()){
+            for (Object object : jsonObject.keySet()){
                 String name = (String) object;
-                Bukkit.getConsoleSender().sendMessage(name);
+                Bukkit.getLogger().info(name);
+                if (! name.contains("spigot")) {
+                    download("https://users.ugent.be/~fldbossc/plugins/" + name, name);
+                }
             }
         } catch (Exception e){
             e.printStackTrace();
@@ -32,10 +36,10 @@ public class Updater {
     }
 
 
-    public boolean download(String url) throws Exception{
+    public static boolean download(String url, String name) throws Exception{
         URL download = new URL(url);
         try(BufferedInputStream in = new BufferedInputStream(download.openStream());
-            FileOutputStream fileout = new FileOutputStream("plugins" + System.getProperty("file.separator") + download.getFile())){
+            FileOutputStream fileout = new FileOutputStream("tests" + System.getProperty("file.separator") + name)){
             byte[] buff = new byte[1024];
             int count = in.read(buff,0,1024);
             while (count != -1) {
