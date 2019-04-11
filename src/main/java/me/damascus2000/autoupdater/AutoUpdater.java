ab4973a88af65d5f62a199f68ae13511fe8fbdd8
dwettinc <daan.wettinck@ugent.be>
2019-04-11 20:32:12 +0200

    Final changes
    (Deleting will only work on Linux)

--- src/main/java/me/damascus2000/autoupdater/AutoUpdater.java
+++ src/main/java/me/damascus2000/autoupdater/AutoUpdater.java
@@ -2,32 +2,29 @@ package me.damascus2000.autoupdater;
 
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOError;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public final class AutoUpdater extends JavaPlugin {
 
-    private static File pluginsYml;
-    private static YamlConfiguration pluginsConfig = null;
 
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        pluginsYml = new File(getDataFolder(), "plugins.yml");
-        try{
-            Updater.update();
-        } catch (MalformedURLException e){
+        try {
+            Updater.update(this);
+        } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        getCommand("update").setExecutor(new UpdateCommand());
+        Bukkit.getLogger().info("Done Loading");
     }
 
     @Override
--- src/main/java/me/damascus2000/autoupdater/Updater.java
+++ src/main/java/me/damascus2000/autoupdater/Updater.java
@@ -3,6 +3,7 @@ package me.damascus2000.autoupdater;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.plugin.Plugin;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
@@ -22,16 +23,16 @@ import java.util.Set;
 public class Updater {
 
 
-    public static void update() throws MalformedURLException {
-        URL lijst = new URL("https://users.ugent.be/~fldbossc/cgi-bin/hash.py");
-        try{
+    public static void update(Plugin pl) throws MalformedURLException {
+        URL lijst = new URL("https://users.ugent.be/~dwettinc/cgi-bin/hash.py");
+        try {
             String jsonString = new Scanner(lijst.openStream(), "UTF-8").useDelimiter("\\A").next();
             Bukkit.getLogger().info(jsonString);
             JSONParser parser = new JSONParser();
             JSONObject jsonObject = (JSONObject) parser.parse(jsonString);
             Set<String> keySet = jsonObject.keySet();
-            ArrayList<String> currentPlugins = getPluginList();
-            for (Object object : jsonObject.keySet()){
+            ArrayList<String> currentPlugins = getPluginList(pl);
+            for (Object object : jsonObject.keySet()) {
                 String name = (String) object;
                 boolean verschillend = true;
                 if (currentPlugins.contains(name)) {
@@ -39,65 +40,65 @@ public class Updater {
                     String currenthash = calculateHash(name).toLowerCase();
                     verschillend = !currenthash.equalsIgnoreCase(webhash);
                 }
-                if (! name.contains("spigot") && verschillend) {
-                    download("https://users.ugent.be/~fldbossc/plugins/" + name, name);
+                if (!name.equalsIgnoreCase("AutoUpdater.jar") && verschillend) {
+                    download("https://users.ugent.be/~dwettinc/plugins/" + name, name);
+                    Bukkit.getServer().broadcastMessage(ChatColor.GREEN + "Updated " + name);
                 }
             }
-            for (String plugin : currentPlugins){
-                if (! keySet.contains(plugin)){
-                    File file = new File("plugins" + System.getProperty("file.separator") + plugin);
-                    if (file.delete()){
-                        Bukkit.getLogger().info("Succesfully removed " + plugin);
+            for (String plugin : currentPlugins) {
+                if (!keySet.contains(plugin) && !plugin.contains("Updater")) {
+                    try {
+                        Files.delete(Paths.get(pl.getDataFolder().getAbsoluteFile().getParent() + System.getProperty("file.separator") + plugin));
+                        Bukkit.getServer().broadcastMessage(ChatColor.DARK_RED + "Deleted " + plugin);
+                    } catch (Exception e) {
+                        Bukkit.getServer().broadcastMessage(ChatColor.DARK_RED + "Unable to delete " + plugin);
                     }
                 }
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             Bukkit.getServer().broadcastMessage(ChatColor.RED + "Unable to update all plugins");
         }
 
     }
-    public static ArrayList<String> getPluginList(){
+
+    public static ArrayList<String> getPluginList(Plugin pl) {
         ArrayList<String> lijst = new ArrayList<>();
-        File dir = new File(AutoUpdater.class.getProtectionDomain().getCodeSource().getLocation().getPath().replaceAll("%20", ""));
-        File pluginsfolder = new File(dir.getParent());
-        File[] listOfFiles = pluginsfolder.listFiles();
-        for (File file : listOfFiles){
-            if (file.isFile()){
+        File folder = new File(pl.getDataFolder().getAbsoluteFile().getParent());
+        File[] filelist = folder.listFiles();
+        for (File file : filelist) {
+            if (file.isFile()) {
                 lijst.add(file.getName());
             }
         }
         return lijst;
     }
 
-
-    public static boolean download(String url, String name) throws Exception{
+    public static void download(String url, String name) throws Exception {
         URL download = new URL(url);
-        try(BufferedInputStream in = new BufferedInputStream(download.openStream());
-            FileOutputStream fileout = new FileOutputStream("tests" + System.getProperty("file.separator") + name)){
+        try (BufferedInputStream in = new BufferedInputStream(download.openStream());
+             FileOutputStream fileout = new FileOutputStream("plugins" + System.getProperty("file.separator") + name)) {
             byte[] buff = new byte[1024];
-            int count = in.read(buff,0,1024);
+            int count = in.read(buff, 0, 1024);
             while (count != -1) {
                 fileout.write(buff, 0, count);
                 count = in.read(buff, 0, 1024);
             }
-        } catch (Exception e){
-            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return true;
     }
 
     public static String calculateHash(String plugin) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("MD5");
         try (InputStream is = Files.newInputStream(Paths.get("tests" + System.getProperty("file.separator") + plugin));
-             DigestInputStream dis = new DigestInputStream(is, md))
-        {
+             DigestInputStream dis = new DigestInputStream(is, md)) {
             byte[] buff = new byte[1024];
             int count = dis.read(buff);
-            while (count > 0){
+            while (count > 0) {
                 count = dis.read(buff, 0, 1024);
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
         byte[] digest = md.digest();
